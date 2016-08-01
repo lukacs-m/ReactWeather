b3ee142542a1958999f487ff687f9c17ccd4143d
martin <martin.lukacs@epitech.eu>
2016-08-02 00:07:39 +0200

    css implementation

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h2 className="text-center">About</h2>
+      <h2 className="text-center page-title">About</h2>
       <p>This is a weather application builton react</p>
     </div>
   );
